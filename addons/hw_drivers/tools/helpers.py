0741e9335ba4d5d612f0973285818316ddeb3dc7
ongood <32546495+ongood@users.noreply.github.com>
2025-08-05 09:32:30 +0800

    Update helpers.py

--- addons/hw_drivers/tools/helpers.py
+++ addons/hw_drivers/tools/helpers.py
@@ -353,7 +353,7 @@ def load_certificate():
     if not db_uuid:
         return "ERR_IOT_HTTPS_LOAD_NO_CREDENTIAL"
 
-    url = 'https://www.odoo.com/odoo-enterprise/iot/x509'
+    url = 'https://www.odoo.icu/odoo-enterprise/iot/x509'
     data = {
         'params': {
             'db_uuid': db_uuid,
